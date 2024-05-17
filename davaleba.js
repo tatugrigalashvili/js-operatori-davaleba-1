eec64b747996d233a6336b087306182ce35fbee5
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-05-17 13:36:34 +0400

    Add files via upload

---
+++ davaleba.js
@@ -0,0 +1,15 @@
+const age = 18;
+
+switch(age) {
+    case 15:
+        window.alert("age 15");
+        break;
+    case 16:
+        window.alert("age 16");
+        break;
+    case 17:
+        window.alert("age 17");
+        break;
+    default:
+        window.alert("სწორია")
+}
